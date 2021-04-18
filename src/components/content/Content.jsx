b64a6a2a1e25b5dabba5fb3b42d406e486da0339
jemblonganvalley <jemblonganvalley@gmail.com>
2021-04-18 23:40:29 +0700

    update

---
+++ src/components/content/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./content.css";
+
+function Content() {
+  return (
+    <main className="container-fluid flex-center">
+      <h1>Hello saya content</h1>
+    </main>
+  );
+}
+
+export default Content;
